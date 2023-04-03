de6f950ab9aa8505d372a6d7db4425b1455ee88b
lomlomm <vanessaserna60@gmail.com>
2023-04-03 17:42:16 -0600

    Add operators functions

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 const display = document.querySelector(".displaytxt");
 const buttons = document.querySelectorAll(".number p");
-
 buttons.forEach( (button) =>  {
     button.addEventListener("click", () => {
         display.textContent+=`${button.textContent}`
     })
 })
 
-function operator(){
-    const operators = document.querySelectorAll(".operators .number");
-    operators.forEach((operator) => {
-        operator.addEventListener("click", () => {
-            var selected = operator.textContent
-        })
+const operators = document.querySelectorAll(".operators .number p");
+var selected = "prueba";
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        selected = operator.textContent;
     })
-}
+})
 
 function result(){
     const equals = document.querySelector("#equal");
     equals.addEventListener("click", () => {
-        var text = display.textContent
-        selected 
-
+        var numbers = display.textContent.split(selected)
+        var result;
+        switch (selected) {
+            case "+":
+                result = parseFloat(numbers[0]) + parseFloat(numbers[1])
+                break;
+            case "-":
+                result = parseFloat(numbers[0]) - parseFloat(numbers[1])
+                break;
+            case "/":
+                result = parseFloat(numbers[0]) / parseFloat(numbers[1])
+                break;
+            case "x":
+                result = parseFloat(numbers[0]) * parseFloat(numbers[1])
+                break;
+        }
+        display.textContent = result; 
     })
 }
 
